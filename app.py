a1485f96b00b90d88ccf57a82d11555488a15a7d
Marshall Cunningham <mcunningham@sigmapoint.com>
2020-10-04 19:09:12 -0400

    Completed and commented code

--- app.py
+++ app.py
@@ -10,29 +10,30 @@ import time
 import os
 import json
 
+# Create webserver
 server = flask.Flask('app')
 server.secret_key = os.environ.get('secret_key', 'secret')
 
-
+# Load transit data and remove data with non-functioning GPS coordinates
 transit_data = pd.read_csv("datasets\\Transit Data - October.csv")
 print(transit_data.shape)
 transit_data = transit_data[transit_data.Latitude > 44]
 print(transit_data.shape)
 transit_data = transit_data[transit_data.Longitude < -76]
 print(transit_data.shape)
+# Groupby COordinates to reduce number of discete points
 clean = transit_data.groupby(['Latitude', 'Longitude']).size().reset_index(name='counts')
 print(clean.head())
 print(clean.shape)
 
 
 
-
+# Load geojson files for the background layers
 with open("A1_geo\\neighbourhoods.geojson", "r") as geo:
     neighbourhood_geojson = json.load(geo)
 with open("A1_geo\\transit-gtfs-routes.geojson", "r") as geo:
     routes_geojson = json.load(geo)
-# with open("A1_geo\\transit_data.geojson", "r") as geo:
-#     transit_data_geojson = json.load(geo)
+# Create list of colors to use when generating neighbourhood layers
 colors = [
     "firebrick",
     "floralwhite",
@@ -97,10 +98,9 @@ colors = [
     "orange",
     "orangered"
 ]
-layers = []
 
-print(len(neighbourhood_geojson['features']))
-print(len(colors))
+layers = []
+# Create a list of features for each neighbourhood (to color each individually)
 for n, neighbourhood in enumerate(neighbourhood_geojson['features']):
     layers.append(
         {
@@ -112,6 +112,7 @@ for n, neighbourhood in enumerate(neighbourhood_geojson['features']):
             'opacity': 0.6
         }
     )
+# Add the transit routes geojson to the layers list
 layers.append(
     {
         'visible': True,
@@ -122,15 +123,14 @@ layers.append(
         'opacity': 1
     }
 )
+# Create a plotly Densitymapbox plot for the transit locations
 fig = go.Figure(go.Densitymapbox(
-    # mode = "markers",
     lon = clean.Longitude,
     lat = clean.Latitude,
     z=clean.counts,
     radius=15
-    # marker = {'size': 3, 'color': "cyan"}
     ))
-
+# Specify stylistic preferences and add in background layers (neighbourhoods and transit routes)
 fig.update_layout(
     height=700,
     mapbox = {
@@ -138,45 +138,21 @@ fig.update_layout(
         'center': {"lat": 44.227860, "lon": -76.496938},
         'zoom': 10,
         'layers': layers
-                # {
-                #     'visible': True,
-                #     'source': neighbourhood_geojson['features'][0],
-                #     'type': "fill",
-                #     'below': "traces",
-                #     'color': "royalblue",
-                #     'opacity': 0.6
-                # },
-                # {
-                #     'visible': True,
-                #     'source': routes_geojson,
-                #     'type': "line",
-                #     'below': "traces",
-                #     'color': "green",
-                #     'opacity': 0.8
-                # },
-                # {
-                #     'visible': True,
-                #     'source': transit_data_geojson,
-                #     'type': "circle",
-                #     'color': "pink",
-                #     'opacity': 1
-                # }
-            # ]
             },
     margin = {'l':0, 'r':0, 'b':0, 't':0})
+# Use more efficient rendering widget
 f = go.FigureWidget(fig)
-
+# Create dash app
 app = dash.Dash('app', server=server)
-
 app.scripts.config.serve_locally = False
 dcc._js_dist[0]['external_url'] = 'https://cdn.plot.ly/plotly-basic-latest.min.js'
-
+# Create the basic layout of the dash app
 app.layout = html.Div([
     html.H1('Transit Data Heatmap'),
     dcc.Graph(id='my-graph', figure=f)
 ], className="container")
 
-
+# Run dash app
 if __name__ == '__main__':
     app.run_server()
 
